704f9a98b03fc032d32829f1b71001ff77e0f2e1
Skylar Weaver <sweaver@andrew.cmu.edu>
2015-03-04 20:18:10 -0500

    Parse should be all set up. just need to have facebook setup so i can test the app

--- chargeme/AppDelegate.swift
+++ chargeme/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,11 +17,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
         FBLoginView.self
         FBProfilePictureView.self
-        return true
+
+
+        Parse.setApplicationId("I7unmJdF7zHeB0erTmiZG1N7VMx7yU27FvjCvHTv", clientKey: "VdtIQrnF9j1ybeiiPfhN0mHm7vYBVSNETCqJnbO5")
+        
+        //Tests to see if parse is working
+        var object = PFObject(className: "TestClass")
+        object.addObject("Banana", forKey: "favoriteFood")
+        object.addObject("Chocolate", forKey: "favoriteIceCream")
+        object.save()
+        
+        
+//        // [Optional] Power your app with Local Datastore. For more info, go to
+//        // https://parse.com/docs/ios_guide#localdatastore/iOS
+//        [Parse enableLocalDatastore];
+//        
+//        // Initialize Parse.
+//        [Parse setApplicationId:@"I7unmJdF7zHeB0erTmiZG1N7VMx7yU27FvjCvHTv"
+//        clientKey:@"VdtIQrnF9j1ybeiiPfhN0mHm7vYBVSNETCqJnbO5"];
+//        
+//        // [Optional] Track statistics around application opens.
+//        [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+//        
+//        // ...
+
+    return true
     }
+    
     
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString?, annotation: AnyObject) -> Bool {
         
@@ -49,21 +74,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // [Optional] Power your app with Local Datastore. For more info, go to
-    // https://parse.com/docs/ios_guide#localdatastore/iOS
-    [Parse enableLocalDatastore];
-    
-    // Initialize Parse.
-    [Parse setApplicationId:@"I7unmJdF7zHeB0erTmiZG1N7VMx7yU27FvjCvHTv"
-    clientKey:@"VdtIQrnF9j1ybeiiPfhN0mHm7vYBVSNETCqJnbO5"];
-    
-    // [Optional] Track statistics around application opens.
-    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
-    
-    // ...
     }
 
 }
---
+++ chargeme/parseBridgerHeader.m
@@ -0,0 +1,9 @@
+//
+//  parseBridgerHeader.m
+//  chargeme
+//
+//  Created by Skylar Weaver on 3/4/15.
+//  Copyright (c) 2015 Paul Okuda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
