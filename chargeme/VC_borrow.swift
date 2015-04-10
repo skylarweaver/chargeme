c6a9bb85668dcfdc7db0d2afe801b0ba280be033
Skylar Weaver <sweaver@andrew.cmu.edu>
2015-04-09 21:14:26 -0400

    Starting requester class

--- chargeme/VC_borrow.swift
+++ chargeme/VC_borrow.swift
@@ -12,9 +12,27 @@ class VC_borrow: UIViewController {
     @IBOutlet weak var deviceTextField: UITextField!
     @IBOutlet weak var timeSlider: UISlider!
     
-    @IBAction func findLenderButton(sender: AnyObject) {
+    @IBAction func findLenderButton(sender: AnyObject)
+        {
         // Create request class
-        // save device and time duration to parse in that request object, along with user requesting
+            var object = PFObject(className: "Request")
+            object.addObject(deviceTextField.text, forKey: "websiteUrl")
+            object.addObject("Five", forKey: "websiteRating")
+            object.saveInBackgroundWithBlock {
+                (success: Bool!, error: NSError!) -> Void in
+                if (success != nil) {
+                    NSLog("Object created with id: \(object.objectId)")
+                } else {
+                    NSLog("%@", error)
+                }
+                var user2 = PFUser.currentUser()
+                if (user2 != nil){
+                    println("EMAIL!")
+                    println(user2.email)
+                }
+            }
+
+            // save device and time duration to parse in that request object, along with user requesting
         //search for other users who have that charger in order of distance. Might want another function or class to do this?
         //
         
